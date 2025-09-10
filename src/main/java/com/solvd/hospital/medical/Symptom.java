85c6da0fecce2234dc98c2e04f076afb41cc4ce5
aAlejandroNM <adrian.neira@uptc.edu.co>
2025-09-10 11:44:53 -0500

    feature replace data classes with records

--- src/main/java/com/solvd/hospital/medical/Symptom.java
+++ src/main/java/com/solvd/hospital/medical/Symptom.java
@@ -3,20 +3,15 @@ package com.solvd.hospital.medical;
 import com.solvd.hospital.model.MedicalEntity;
 import com.solvd.hospital.model.SymptomSeverity;
 
-public class Symptom extends MedicalEntity {
-    private SymptomSeverity severity;
-
-    public Symptom(String name, String description, SymptomSeverity severity) {
-        super(name, description);
-        this.severity = severity;
+public record Symptom(String name, String description, SymptomSeverity severity) implements MedicalEntity {
+    @Override
+    public String getName() {
+        return name;
     }
 
-    public SymptomSeverity getSeverity() {
-        return severity;
-    }
-
-    public void setSeverity(SymptomSeverity severity) {
-        this.severity = severity;
+    @Override
+    public String getDescription() {
+        return description;
     }
 
     @Override
@@ -24,12 +19,14 @@ public class Symptom extends MedicalEntity {
         return "Symptom";
     }
 
-    @Override
     public String toString() {
         return "Symptom{" +
-                "name='" + getName() + '\'' +
-                ", description='" + getDescription() + '\'' +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
                 ", severity=" + severity +
                 '}';
+    }
+    public SymptomSeverity getSeverity() {
+        return severity;
     }
 }
--- src/main/java/com/solvd/hospital/model/MedicalEntity.java
+++ src/main/java/com/solvd/hospital/model/MedicalEntity.java
@@ -1,37 +1,7 @@
 package com.solvd.hospital.model;
 
-public abstract class MedicalEntity {
-    protected String name;
-    protected String description;
-
-    public MedicalEntity(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public abstract String getEntityType();
-
-    @Override
-    public String toString() {
-        return "MedicalEntity{" +
-                "name='" + getName() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                '}';
-    }
-} 
+public interface MedicalEntity {
+    String getName();
+    String getDescription();
+    String getEntityType();
+}
